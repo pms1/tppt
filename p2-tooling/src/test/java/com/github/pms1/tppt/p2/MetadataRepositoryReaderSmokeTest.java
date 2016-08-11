b8a6849b57e8e71e9a825a13d64d5a16043e21e6
Mirko Streckenbach <pms1@users.noreply.github.com>
2016-08-11 07:20:44 +0200

    work on #1: refine xsd for metadata, add smoke tests

---
+++ p2-tooling/src/test/java/com/github/pms1/tppt/p2/MetadataRepositoryReaderSmokeTest.java
@@ -0,0 +1,43 @@
+package com.github.pms1.tppt.p2;
+
+import java.io.InputStream;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MetadataRepositoryReaderSmokeTest {
+
+	@Rule
+	public TestResourceRule rule;
+
+	@Rule
+	public PlexusContainerRule plexusContainer = new PlexusContainerRule();
+
+	private final String contentXml;
+
+	@Parameters(name = "{0}")
+	public static Object[] parameters() {
+		return new Object[] { "contentEmpty.xml", "contentOtherOrder.xml", "contentLicensesCopyright.xml",
+				"contentReferences.xml", "contentLicenseNoAttributes.xml", "contentPatch.xml",
+				"contentHostRequirementsGenerationMatch.xml", "contentNoProperties.xml", "contentNoTouchpoint.xml",
+				"contentMultiple.xml", "contentAttributeDescription.xml", "contentOrderReferenceNoUriUrl.xml",
+				"contentNoProvides.xml", "contentPropertiesNoSize.xml", "contentMetaRequirements.xml" };
+	}
+
+	public MetadataRepositoryReaderSmokeTest(String contentXml) {
+		this.contentXml = contentXml;
+	}
+
+	@Test
+	public void read() throws Exception {
+		MetadataRepositoryFactory lookup = plexusContainer.lookup(MetadataRepositoryFactory.class);
+		try (InputStream in = getClass().getResourceAsStream(contentXml)) {
+			assert in != null;
+			lookup.readStream(in);
+		}
+	}
+}
