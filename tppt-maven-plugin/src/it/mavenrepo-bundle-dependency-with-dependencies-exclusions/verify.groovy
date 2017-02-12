a730f04f4dc33e7bf8d4e48112a217f4e96f1fcf
Mirko Streckenbach <pms1@users.noreply.github.com>
2017-02-12 12:00:18 +0100

    #6 test case for <exclusions>

--- tppt-maven-plugin/src/it/mavenrepo-bundle-dependency-with-dependencies-exclusions/verify.groovy
+++ tppt-maven-plugin/src/it/mavenrepo-bundle-dependency-with-dependencies-exclusions/verify.groovy
@@ -3,7 +3,7 @@ import java.util.zip.ZipFile
 def zf = new ZipFile(new File(basedir,"target/test-0.0.1-SNAPSHOT.zip"));
 try {
 	assert zf.getEntry("plugins/org.glassfish.jersey.core.jersey-client_2.23.1.jar")
-	assert zf.getEntry("plugins/org.glassfish.jersey.core.jersey-common_2.23.1.jar")
+	assert !zf.getEntry("plugins/org.glassfish.jersey.core.jersey-common_2.23.1.jar")
 	assert zf.getEntry("plugins/org.glassfish.hk2.osgi-resource-locator_1.0.1.jar")
 } finally {
 	zf.close();
