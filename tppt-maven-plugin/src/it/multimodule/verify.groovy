0bc20e486cedb25e826d434afaba94b0742f6c03
Mirko Streckenbach <pms1@users.noreply.github.com>
2017-03-10 05:41:22 +0100

    #8 test duplicate deployment; define deploymentTarget in project

--- tppt-maven-plugin/src/it/multimodule/verify.groovy
+++ tppt-maven-plugin/src/it/multimodule/verify.groovy
@@ -1,8 +1,8 @@
 import java.util.zip.ZipFile
 
-def zf = new ZipFile(new File(basedir,"repository/target/test-0.0.1-SNAPSHOT.zip"));
+def zf = new ZipFile(new File(basedir,"repository/target/repository-0.0.0-SNAPSHOT.zip"));
 try {
-	assert zf.getEntry("plugins/my-osgi-bundles.examplebundle_0.0.1.SNAPSHOT.jar")
+	assert zf.getEntry("plugins/com.github.pms1.tppt.bundle1_0.0.0.SNAPSHOT.jar")
 	assert zf.getEntry("plugins/org.osgi.core_4.0.0.jar")
 } finally {
 	zf.close();
