185f3056bce507f81fc4585387a8218ef4827e4c
Mirko Streckenbach <pms1@users.noreply.github.com>
2017-03-30 05:22:11 +0200

    fixes #16 copy filter from metadata to created feature.xml
    (cherry picked from commit 4b3ccf5357320732369acb716afa8680bf0b8255)

--- tppt-maven-plugin/src/main/java/com/github/pms1/tppt/CreateFeaturesMojo.java
+++ tppt-maven-plugin/src/main/java/com/github/pms1/tppt/CreateFeaturesMojo.java
@@ -202,6 +202,8 @@ public class CreateFeaturesMojo extends AbstractMojo {
 				if (!provided.isPresent())
 					continue;
 
+				u.getFilter();
+
 				Optional<Provided> fragment = u.getProvides().getProvided().stream()
 						.filter(p -> p.getNamespace().equals("osgi.fragment")).findAny();
 
@@ -218,6 +220,7 @@ public class CreateFeaturesMojo extends AbstractMojo {
 				if (fragment.isPresent())
 					p.fragment = true;
 				p.unpack = false;
+				p.filter = u.getFilter();
 				plugins.add(p);
 			}
 
