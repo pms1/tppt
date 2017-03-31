ed11db1c2c15de9aa0c9d7a049038b8feb92b4c3
Mirko Streckenbach <pms1@users.noreply.github.com>
2017-03-31 05:17:28 +0200

    add missing test

--- tppt-maven-plugin/src/it/shortVersion/verify.groovy
+++ tppt-maven-plugin/src/it/shortVersion/verify.groovy
@@ -1,2 +1,14 @@
-println "FIXME"
+import java.util.zip.ZipFile
+
+def zf = new ZipFile(new File(basedir,"target/shortVersion-1.0.zip"));
+try {
+	assert zf.getEntry("features/com.github.pms1.tppt.shortVersion_1.0.0.100.jar")
+} finally {
+	zf.close();
+}
+
+def buildLog = new File(basedir, 'build.log').readLines();
+
+assert buildLog.grep { it.contains('Deploying to ') }.size() == 2
+assert buildLog.grep { it.contains(' Equal to existing repository, skipping deployment') }.size() == 1
 	
