d947fb3c6cc5a84f3ea620ebdf71ffba5a4b0092
Mirko Streckenbach <pms1@users.noreply.github.com>
2018-06-16 07:03:27 +0200

    disable site-deploy since it breaks in travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-mvn -Prelease deploy site-deploy -s .travis/settings.xml -DskipTests=true -Dinvoker.skip=true -B
+# site-deploy does not work due to outdated ssh algorithms in wagoon-git. need to fix later.
+# mvn -Prelease deploy site-deploy -s .travis/settings.xml -DskipTests=true -Dinvoker.skip=true -B
+mvn -Prelease deploy -s .travis/settings.xml -DskipTests=true -Dinvoker.skip=true -B
