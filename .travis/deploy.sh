3a3d76c7f4137bb8e75c754ac1ca95ac0ad31036
Mirko Streckenbach <pms1@users.noreply.github.com>
2020-06-04 10:41:17 +0200

    use github actions instead of travis

--- .travis/deploy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# site-deploy does not work due to outdated ssh algorithms in wagoon-git. need to fix later.
-# mvn -Prelease deploy site-deploy -s .travis/settings.xml -DskipTests=true -Dinvoker.skip=true -B
-mvn -Prelease deploy -s .travis/settings.xml -DskipTests=true -Dinvoker.skip=true -B
