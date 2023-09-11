d4964c90f88665a03a9a1b32ecb666ce2cd119a2
Mirko Streckenbach <pms1@users.noreply.github.com>
2023-09-11 11:13:24 +0200

    use a recent version of eclipse for mirroring tests

--- tppt-maven-plugin/src/it/mirror/verify.groovy
+++ tppt-maven-plugin/src/it/mirror/verify.groovy
@@ -3,8 +3,8 @@ import java.util.zip.ZipFile
 def zf = new ZipFile(new File(basedir,"target/mirror-0.0.0-SNAPSHOT.zip"));
 try {
 	assert zf.getEntry("plugins/net.sf.jopt-simple.jopt-simple_5.0.1.jar")
-	assert zf.getEntry("plugins/org.eclipse.osgi_3.12.50.v20170928-1321.jar")
-	assert !zf.getEntry("plugins/org.eclipse.osgi_3.12.1.v20170821-1548.jar")
+	assert zf.getEntry("plugins/org.eclipse.osgi_3.18.400.v20230509-2241.jar")
+	assert !zf.getEntry("plugins/org.eclipse.core.jobs_3.14.0.v20230317-0901.jar")
 } finally {
 	zf.close();
 }
